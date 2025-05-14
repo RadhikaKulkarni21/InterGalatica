68b03f732fec4ba5e7bc25d2d7a51d6f53425a36
radhi <radhika2103@gmail.com>
2025-05-14 19:43:07 +0100

    Input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    Vector2 rawInput;
+    [SerializeField] float moveSpeed = 5f;
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        //For continous movement instead of move on press
+        //Time.deltaTime is the last frame took to get processed, making it frame rate independant
+        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
+        transform.position += delta;
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log(rawInput);
     }
 }
