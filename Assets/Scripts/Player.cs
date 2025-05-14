56e5603f2f0874e1a27a6f2bc91aedbe84e3dfdc
radhi <radhika2103@gmail.com>
2025-05-14 20:28:24 +0100

    Player Boundries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,39 @@ public class Player : MonoBehaviour
 {
     Vector2 rawInput;
     [SerializeField] float moveSpeed = 5f;
+
+    Vector2 minBound;
+    Vector2 maxBound;
+
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+    void Start()
+    {
+        InitBound();
+    }
     void Update()
     {
         Move();
+    }
+
+    void InitBound()
+    {
+        Camera mainCamera = Camera.main;
+        minBound = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
+        maxBound = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
     }
 
     void Move()
     {
         //For continous movement instead of move on press
         //Time.deltaTime is the last frame took to get processed, making it frame rate independant
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x,minBound.x + paddingLeft, maxBound.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y,minBound.y + paddingTop, maxBound.y - paddingBottom);
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value)
