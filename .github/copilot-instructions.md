1851d2f5fa58a416cd7fe988b9610696ec59cd42
radhi <radhika2103@gmail.com>
2025-05-19 22:55:46 +0100

    Score keeper pt 1

---
+++ .github/copilot-instructions.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: InterGalatica
+- Unity version: Unity 6000.0.47f1
+- Active game object:
+  - Name: Projectile_Player
+  - Tag: Untagged
+  - Layer: Player
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,18 +3,23 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] bool isPlayer;
     [SerializeField] int health = 50;
+    [SerializeField] int score = 50;
     [SerializeField] ParticleSystem hitEffect;
 
     [SerializeField] bool applyCameraShake;
     CameraShake cameraShake;
     AudioPlayer audioPlayer;
+    ScoreKeeper scoreKeeper;
 
     void Awake()
     {
         cameraShake = Camera.main.GetComponent<CameraShake>();
         audioPlayer = FindAnyObjectByType<AudioPlayer>();
+        scoreKeeper = FindAnyObjectByType<ScoreKeeper>();
     }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         DamageDealer damageDealer = collision.GetComponent<DamageDealer>();
@@ -40,14 +45,28 @@ public class Health : MonoBehaviour
         }
     }
 
+    public int GetHealth()
+    {
+        return health;
+    }
+
     void TakeDamage(int damage)
     {
         health -= damage;
         if(health <= 0)
         {
-            Destroy(gameObject);
+            Die();
         }
     }
+    void Die()
+    {
+        if (!isPlayer)
+        {
+            scoreKeeper.ModifyScore(score);
+        }
+        Destroy(gameObject);
+    }
+
     void ShakeCamera()
     {
         if(cameraShake != null && applyCameraShake)
